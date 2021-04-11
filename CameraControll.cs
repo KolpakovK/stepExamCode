8fe167c8878ba0b8331b9a01b8d2202db4dd5c80
KolpakovK <71520079+KolpakovK@users.noreply.github.com>
2021-04-11 10:38:01 +0300

    Add files via upload

---
+++ CameraControll.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControll : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+    [SerializeField]
+    private Vector3 offsetToTarget;
+    [SerializeField]
+    private float cameraMovementSpeed = 4f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target != null)
+        {
+            transform.position = Vector3.Lerp(transform.position,target.position+offsetToTarget,cameraMovementSpeed*Time.deltaTime);
+        }
+    }
+}
---
+++ playerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+
+    public float Speed = 3f;
+    public float jumpHeight = 3f;
+    public float RotationSpeed = 10f;
+
+    CharacterController controller;
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 directionMove = new Vector3(0,0, Input.GetAxisRaw("Vertical"));
+        transform.Rotate(transform.up * Input.GetAxisRaw("Horizontal") * RotationSpeed * Time.deltaTime);
+
+        directionMove = transform.TransformVector(directionMove);
+
+        if (Input.GetButtonDown("Jump") && controller.isGrounded)
+        {
+            directionMove.y += Mathf.Sqrt(jumpHeight * 3.0f);
+        }
+
+        if (directionMove!= Vector3.zero)
+        {
+            animator.SetBool("Run",true);
+        }
+        else
+        {
+            animator.SetBool("Run", false);
+        }
+
+        controller.Move(directionMove * Speed * Time.deltaTime);
+    }
+}
